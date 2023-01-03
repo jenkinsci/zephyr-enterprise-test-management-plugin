8136af6033e67a71f8177140372269c20a91660b
Krzysztof Agienczuk <krzysztof.agienczuk@smartbear.com>
2023-01-03 15:05:42 +0100

    Add accessType header to login request and update pom.xml to use https for svninfra resource
    ZENG-1090
    ZEPHYR-34689
    
    Co-authored-by: Slawomir Oleksy <slawomir.oleksy@smartbear.com>
    Co-authored-by: Jakub Pawlak <jakub.pawlak@smartbear.com>

--- src/main/java/com/thed/service/impl/ZephyrRestServiceImpl.java
+++ src/main/java/com/thed/service/impl/ZephyrRestServiceImpl.java
@@ -139,6 +139,7 @@ public class ZephyrRestServiceImpl implements ZephyrRestService {
         httpClientService.clear();
         String encoding = Base64.getEncoder().encodeToString((username+":"+password).getBytes());
         httpClientService.getHeaders().add(new BasicHeader("Authorization", "Basic "+encoding));
+        httpClientService.getHeaders().add(new BasicHeader("accessType", "HTMLUI"));
         String res = httpClientService.getRequest(url);
         httpClientService.getHeaders().clear();
         if(res != null) {
